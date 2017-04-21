a4f5dea40a44ade2ebb4ee8cadf0ba7a9359c57b
Piehr <piehrweb@gmail.com>
2017-04-21 13:22:11 +0200

    Bios management

--- update.sh
+++ update.sh
@@ -4,7 +4,12 @@ BACKUPDIR="/storage/backup/"$_now"/"
 DATFILESBACKUPDIR="dat_files/"
 DATFILESMASTERDIR="dat_files/"
 DATFILESDIR="/storage/.kodi/userdata/addon_data/plugin.program.iarl/dat_files/"
+
+BIOSBACKUPDIR="bios/"
+BIOSMASTERDIR="roms/bios/"
+
 BIOSDIR="/storage/roms/bios/"
+
 echo "!!!WARNING!!!
 You're about to install our up-to-date Git Package (https://github.com/Piehr/RetroLibre) on this computer, updating its content on your system."
 while true; do
@@ -44,6 +49,34 @@ while true; do
 				break;;
 				[Nn]* )
 					echo "Your dat_files have not been updated."
+					
+				break;;
+				 * ) echo "Please answer yes or no.";;
+			 	esac
+		done
+		
+		while true; do
+			        read -p "Do you want to replace your BIOS roms by the Master ones?[y/n]" yn
+				case $yn in
+				[Yy]* )
+					#backing-up dat files 
+					echo -n "Backing up your BIOS roms... "
+					if [ ! -d "$BIOSDIR" ]; then 
+						echo "ERROR: $BIOSDIR NOT FOUND";
+					exit; 
+					fi
+					mkdir $BACKUPDIR$BIOSBACKUPDIR &&
+					cp -rf $BIOSDIR* $BACKUPDIR$BIOSBACKUPDIR &&
+					wait
+					echo "done. Your old BIOS roms are located in $BACKUPDIR$BIOSBACKUPDIR";
+					echo -n "Copying Master BIOS roms... "
+					rm -rf $BIOSDIR* &&
+					cp -rf ${PWD}/$BIOSMASTERDIR* $BIOSDIR &&
+				       	wait
+					echo "done"; 	
+				break;;
+				[Nn]* )
+					echo "Your BIOS roms have not been updated."
 					
 				break;;
 				 * ) echo "Please answer yes or no.";;
